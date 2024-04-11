8fc34926325fd6741a7c400e22e6842da559ef00
kobamasa <ee217106@cs.meiji.ac.jp>
2024-04-11 18:23:43 +0900

    add Docker

---
+++ Grad_CAM/compose.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -eu
+
+# スクリプトが存在するディレクトリを基準に、docker-composeコマンドを実行する
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+cd $SCRIPT_DIR
+
+function build() {
+    docker build -t grad_cam_tutorial .
+}
+
+function up() {
+    docker compose up -d "$@"
+}
+
+function exec() {
+    # コンテナ名を引数として受け取る。デフォルトは ml-agents
+    local container_name=${1:-grad_cam_tutorial}
+    docker compose exec $container_name bash
+}
+
+function down() {
+    docker compose down "$@"
+}
+
+function main() {
+    local subcommand=$1
+    if [[ -z "$subcommand" ]]; then
+        echo "Usage: ./compose.sh [build|up|exec|down]"
+        exit 1
+    fi
+
+    # サブコマンドに応じて関数を呼び出す
+    case "$subcommand" in
+        build)
+            build
+            ;;
+        up)
+            shift # 最初の引数（サブコマンド）を除去
+            up "$@"
+            ;;
+        exec)
+            shift
+            exec "$@"
+            ;;
+        down)
+            shift
+            down "$@"
+            ;;
+        *)
+            echo "Invalid command: $subcommand"
+            echo "Usage: ./compose.sh [build|up|exec|down]"
+            exit 1
+            ;;
+    esac
+}
+
+main "$@"
